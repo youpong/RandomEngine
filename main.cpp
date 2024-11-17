7ebb8921dc74eeef6c6102ab36892ece6eab18b6
NAKAJIMA Yusaku <yupong7@gmail.com>
2024-11-17 17:10:36 +0900

    clang-format

--- main.cpp
+++ main.cpp
@@ -5,38 +5,31 @@
 // Linear congruential generator
 class RandomEngine {
 public:
-    using result_type = unsigned int;
+  using result_type = unsigned int;
 
 private:
-    // params from cc65 compiler
-    unsigned int a = 16843009;
-    unsigned int x;
-    unsigned int c = 826366247;
-    // int m;
+  // params from cc65 compiler
+  unsigned int a = 16'843'009;
+  unsigned int x;
+  unsigned int c = 826'366'247;
+  // int m;
 public:
-    RandomEngine(unsigned int seed = 0) : x(seed) {}
-    result_type operator()()
-    {
-        x = a * x + c;
-        return x;
-    }
-    static constexpr result_type min()
-    {
-        return 0;
-    }
-    static constexpr result_type max()
-    {
-        return std::numeric_limits<unsigned int>::max();
-    }
+  RandomEngine(unsigned int seed = 0) : x(seed) {}
+  result_type operator()() {
+    x = a * x + c;
+    return x;
+  }
+  static constexpr result_type min() { return 0; }
+  static constexpr result_type max() {
+    return std::numeric_limits<unsigned int>::max();
+  }
 };
 
-int main()
-{
-    std::uniform_int_distribution<int> d(1, 6);
-    RandomEngine e;
+int main() {
+  std::uniform_int_distribution<int> d(1, 6);
+  RandomEngine e;
 
-    for (auto i = 0; i != 10; ++i)
-    {
-        std::cout << d(e) << " ";
-    }
+  for (auto i = 0; i != 10; ++i) {
+    std::cout << d(e) << " ";
+  }
 }
