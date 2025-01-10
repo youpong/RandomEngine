ddd847d1339a8a9197390acf5d2b5b5e9cbf0e1a
NAKAJIMA Yusaku <yupong7@gmail.com>
2025-01-10 13:05:29 +0900

    tweak clang format settings

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 #include "random_engine.h"
 
 int main() {
-  std::uniform_int_distribution<int> d(1, 6);
-  RandomEngine e;
-  random_engine_requires(e);
+    std::uniform_int_distribution<int> d(1, 6);
+    RandomEngine e;
+    random_engine_requires(e);
 
-  for (auto i = 0; i != 10; ++i) {
-    std::cout << d(e) << " ";
-  }
-  std::cout << std::endl;
+    for (auto i = 0; i != 10; ++i) {
+        std::cout << d(e) << " ";
+    }
+    std::cout << std::endl;
 }
--- random_engine.h
+++ random_engine.h
@@ -4,35 +4,36 @@
 // implemented by Linear congruential generator
 class RandomEngine {
 public:
-  using result_type = unsigned int;
+    using result_type = unsigned int;
 
 private:
-  // params from cc65 compiler
-  unsigned int a = 16'843'009;
-  unsigned int x;
-  unsigned int c = 826'366'247;
-  // int m;
+    // params from cc65 compiler
+    unsigned int a = 16'843'009;
+    unsigned int x;
+    unsigned int c = 826'366'247;
+    // int m;
 public:
-  RandomEngine(unsigned int seed = 0) : x(seed) {}
-  result_type operator()() {
-    x = a * x + c;
-    return x;
-  }
-  static constexpr result_type min() { return 0; }
-  static constexpr result_type max() {
-    return std::numeric_limits<unsigned int>::max();
-  }
+    RandomEngine(unsigned int seed = 0) : x(seed) {}
+    result_type operator()() {
+        x = a * x + c;
+        return x;
+    }
+    static constexpr result_type min() { return 0; }
+    static constexpr result_type max() {
+        return std::numeric_limits<unsigned int>::max();
+    }
 };
 
 template <typename T>
 concept Engine = requires(T t) {
-  { t.min() } -> std::convertible_to<typename T::result_type>;
-  { t.max() } -> std::convertible_to<typename T::result_type>;
-  { t() } -> std::convertible_to<typename T::result_type>;
+    { t.min() } -> std::convertible_to<typename T::result_type>;
+    { t.max() } -> std::convertible_to<typename T::result_type>;
+    { t() } -> std::convertible_to<typename T::result_type>;
 };
 
-template <typename Engine> auto random_engine_requires(Engine &e) {
-  e.min();
-  e.max();
-  e();
+template <typename Engine>
+auto random_engine_requires(Engine &e) {
+    e.min();
+    e.max();
+    e();
 }
