56ff83fc2eeb0a04385dfdd0dd1981ef5b52d914
NAKAJIMA Yusaku <yupong7@gmail.com>
2025-01-10 12:17:05 +0900

    Add random_engine_requires()

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <limits>
 #include <random>
+#include <concepts>
 
-// Linear congruential generator
+// implemented by Linear congruential generator
 class RandomEngine {
 public:
   using result_type = unsigned int;
@@ -25,9 +26,24 @@ public:
   }
 };
 
+template <typename T>
+concept Engine = requires(T t) {
+  { t.min() } -> std::convertible_to<typename T::result_type>;
+  { t.max() } -> std::convertible_to<typename T::result_type>;
+  { t() } -> std::convertible_to<typename T::result_type>;
+};
+
+template <typename Engine>
+auto random_engine_requires(Engine &e) {
+  e.min();
+  e.max();
+  e();
+}
+
 int main() {
   std::uniform_int_distribution<int> d(1, 6);
   RandomEngine e;
+  random_engine_requires(e);
 
   for (auto i = 0; i != 10; ++i) {
     std::cout << d(e) << " ";
